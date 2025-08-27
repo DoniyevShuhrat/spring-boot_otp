66c9ec3bd05ca2f0a4f84ce09eea425872a4d0bc
DoniyevShuhrat <doniyevshuhrat96304@gmail.com>
2025-08-27 17:42:17 +0500

    I added redirection logic in  HomeController

--- src/main/java/org/apis/demootp/HomeController.java
+++ src/main/java/org/apis/demootp/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){
-        return "home";
+    @GetMapping("/")  // Asosiy sahifa uchun (masalan: http://localhost:8080/)
+    public String redirectToDisplayPage(){
+        // Endi u mavjud "displayingOTP.html" fayliga yo'naltiradi
+//        return "streaming-otps"";
+        return "redirect:/streaming-otps";
     }
+//    public String home(){
+//        return "home";
+//    }
 }
